7b769eade26d030244958d72c497a44f130b74c9
alvesribeirodan <daniloalvesjob@gmail.com>
2024-06-25 10:54:24 -0300

    Add files via upload

---
+++ foo.php
@@ -0,0 +1,15 @@
+<?php
+echo $_POST['nome'];
+echo " / ";
+echo $_POST['user'];
+echo " / ";
+echo $_POST['email'];
+echo " / ";
+echo $_POST['cpf'];
+echo " / ";
+echo $_POST['data'];
+echo " / ";
+echo $_POST['sexo'];
+echo " / ";
+echo $_POST['end'];
+?>
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FORMULÁRIO</title>
 </head>
+
 <body>
     <h1>Formulário</h1>
     <form action="foo.php" method="post">
@@ -13,10 +15,15 @@
         E-mail:<input type="email" name="email"><br>
         CPF:<input type="number" name="cpf"><br>
         Data de nascimento:<input type="date" name="data"><br>
-        Sexo:<input type="text" name="sexo"><br>
-        Endereço:<input type="text" name="end"><br>
-        <input type="submit" value="ENVIAR">
+        Sexo: <select name="sexo">
+            <option value="Masculino">Masculino</option>
+            <option value="Feminino">Feminino</option>
+        </select>
+        <br>
+            Endereço:<input type="text" name="end"><br>
+            <input type="submit" value="ENVIAR">
     </form>
 
 </body>
+
 </html>
