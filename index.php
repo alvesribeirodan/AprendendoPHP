d3a33118f0431f8aedc2a808122c006be24d73a0
alvesribeirodan <daniloalvesjob@gmail.com>
2024-06-25 10:49:54 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORMULÁRIO</title>
+</head>
+<body>
+    <h1>Formulário</h1>
+    <form action="foo.php" method="post">
+        Nome:<input type="text" name="nome"><br>
+        Usuário:<input type="text" name="user"><br>
+        E-mail:<input type="email" name="email"><br>
+        CPF:<input type="number" name="cpf"><br>
+        Data de nascimento:<input type="date" name="data"><br>
+        Sexo:<input type="text" name="sexo"><br>
+        Endereço:<input type="text" name="end"><br>
+        <input type="submit" value="ENVIAR">
+    </form>
+
+</body>
+</html>
